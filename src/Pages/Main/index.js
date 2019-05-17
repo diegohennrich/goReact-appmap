7679d203b5c33dc1d25856042179c890a77cfff6
Diego Hennrich <diegohennrich@gmail.com>
2019-05-17 17:24:36 -0300

    first commit

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import MapGL, { Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+class Main extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -29.6620335,
+      longitude: -51.1679283,
+      zoom: 14
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize)
+    this._resize()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize)
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+    })
+  }
+
+  handleMapClick(e) {
+    const [longitude, latitude] = e.lngLat
+
+    alert(`Latitude: ${latitude} \nLongitude: ${longitude}`)
+  }
+
+  render() {
+    return (
+      <MapGL
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={
+          'pk.eyJ1IjoiZGllZ29oZW5ucmljaCIsImEiOiJjanZzZzM0aTgwMXZlNDRwMXd4ZXp1amgyIn0.dsGfaX7hV51ruAHw5nyAgg'
+        }
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-29.6620335}
+          longitude={-51.1679283}
+          onClick={this.handleMapClick}
+          captureClick={true}
+        >
+          <img
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48
+            }}
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt="user"
+          />
+        </Marker>
+      </MapGL>
+    )
+  }
+}
+
+export default Main
